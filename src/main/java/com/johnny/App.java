bafd6d5b2a81f1614f3cc7ef1350836fa081a360
bagus.yanuar613 <bagus.yanuar613@gmail.com>
2022-02-17 12:48:37 +0700

    configuration

--- src/main/java/com/johnny/App.java
+++ src/main/java/com/johnny/App.java
@@ -1,5 +1,7 @@
 package com.johnny;
 
+import com.johnny.view.MainFrame;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        MainFrame m = new MainFrame();
+        m.setVisible(true);
     }
 }
---
+++ src/main/java/com/johnny/entity/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.johnny.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author johnny
+ */
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "username")
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+}
